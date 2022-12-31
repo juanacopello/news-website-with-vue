2d5ece7a55fd29b6804a234e4b932b99825c7c61
Juana Copello <juanacopello@github.com>
2022-12-30 22:38:51 -0300

    estilos noticias

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,46 @@
 <template>
-<div>
- <div class="containerArticulos">
-  <article v-for="(a, index) in articles" :key="index"  class="cardArticulo">
-  <h4>{{articles[index].title}}</h4>
-  <p>{{articles[index].author}}</p>
-  <a :href="articles[index].url" target="_blank">Ver más</a>
-  </article>
+  <div>
+    <div class="containerArticulos">
+      <article v-for="(a, index) in articles" :key="index" class="cardArticulo">
+        <img :src="articles[index].urlToImage" alt="imagen_noticia" />
+        <div class="texto_articulo">
+           <h4>{{ articles[index].title }}</h4>
+        <p><span v-if="articles[index].author !== null">{{ articles[index].author }} 
+          for </span>
+          <a :href="articles[index].url" target="_blank">{{ articles[index].source.name }}</a></p>
+        </div>
+      </article>
+    </div>
+
+    <articleComponent :newsArticles="articles" />
   </div>
-
-  <articleComponent :newsArticles = "articles" />
-  
-</div>
-
 </template>
 
 <script>
-import Axios from 'axios';
-import articleComponent from './components/articleComponent.vue';
+import Axios from "axios";
+import articleComponent from "./components/articleComponent.vue";
 
 export default {
   name: "App",
   components: {
-    articleComponent
+    articleComponent,
   },
   data() {
     return {
-      articles: []
+      articles: [],
     };
   },
-  created(){
-  Axios.get("https://newsapi.org/v2/top-headlines?country=ar&apiKey=d07831122a564646934cbc8636213c50")
-          .then(
-                  response => {
-                      console.log(response.data.articles)
-                      this.articles = response.data.articles
-                  }
-              )
-          .catch(error => {
-            console.log(error)
-          })
+  created() {
+    Axios.get(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=d07831122a564646934cbc8636213c50"
+    )
+      .then((response) => {
+        console.log(response.data.articles);
+        this.articles = response.data.articles;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
   methods: {
     cambiarNombre(nombre, event) {
@@ -59,8 +61,7 @@ export default {
 </script>
 
 <style>
-
-:root{
+:root {
   --first-color: black;
 }
 #app {
@@ -72,17 +73,31 @@ export default {
   margin-top: 60px;
 }
 
-.containerArticulos{
-display: flex;
-flex-direction: column;
+.containerArticulos {
+  display: flex;
+  flex-direction: column;
 }
 
-.containerArticulos .cardArticulo{
-    border-bottom: 1px solid var(--first-color);
+.cardArticulo {
+  border-bottom: 1px solid var(--first-color);
+  display: flex;
+  flex-direction: row;
 }
 
-.containerArticulos .cardArticulo a{
-      color: var(--first-color);
-    text-decoration: none;
+.cardArticulo a {
+  color: var(--first-color);
+  font-weight: 700;
+  
+}
+
+.cardArticulo img{
+  width: 250px;
+  height: 180px;
+}
+
+.texto_articulo{
+  display: flex;
+  flex-direction: column;
+
 }
 </style>
