d5bbb0d595c9a74d786b7d95ae97cad53a473375
Juana Copello <juanacopello@github.com>
2023-01-05 22:19:50 -0300

    change icon, styles

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <h4>{{ articles[index].title.split("-")[0] }}</h4>
           <p>
             <span v-if="articles[index].author !== null"
-              >{{ articles[index].author }} for
+              >{{ articles[index].author }} in
             </span>
             <a :href="articles[index].url" target="_blank">{{
               articles[index].source.name
@@ -17,18 +17,19 @@
       </article>
     </div>
 
-    <articleComponent :newsArticles="articles" />
+    <!-- <articleComponent :newsArticles="articles" /> -->
+    <!-- || articles[index].author.includes('https') == false -->
   </div>
 </template>
 
 <script>
 import Axios from "axios";
-import articleComponent from "./components/articleComponent.vue";
+// import articleComponent from "./components/articleComponent.vue";
 
 export default {
   name: "App",
   components: {
-    articleComponent,
+    // articleComponent,
   },
   data() {
     return {
@@ -42,32 +43,14 @@ export default {
       .then((response) => {
         console.log(response.data.articles);
         this.articles = response.data.articles;
-
-        
-
-
       })
-    
+
       .catch((error) => {
         console.log(error);
       });
   },
-  computed(){},
-  methods: {
-    cambiarNombre(nombre, event) {
-      this.name = nombre;
-      console.log(event);
-    },
-    sumar(num) {
-      this.count += num;
-    },
-    restar(num) {
-      this.count -= num;
-    },
-    debug(value) {
-      console.log(value);
-    },
-  },
+  computed() {},
+  methods: {},
 };
 </script>
 
@@ -90,10 +73,12 @@ export default {
 }
 
 .cardArticulo {
-  border-bottom: 1px solid var(--first-color);
   display: flex;
   flex-direction: row;
   text-align: left;
+  margin: auto;
+  padding: 10px 0;
+  border-bottom: 1px solid black;
 }
 
 .cardArticulo a {
@@ -110,16 +95,18 @@ export default {
 .cardArticulo p {
   padding: 20px 0;
   margin: 0;
+  text-transform: uppercase;
+  font-size: 12px;
 }
 
 .cardArticulo img {
-  width: 250px;
-  height: 180px;
+  width: 200px;
+  height: auto;
 }
 
 .texto_articulo {
   display: flex;
   flex-direction: column;
-  margin:  0 10px;
+  margin: 0 10px;
 }
 </style>
